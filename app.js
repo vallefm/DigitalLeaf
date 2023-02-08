152a75ebcbad48e5d0c02188b0f81f7c29893858
syko99 <sykong.yang@hotmail.com>
2023-02-07 21:53:55 -0600

    progress on database connection. Not yet ready

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const { appendFile } = require('fs')
-const app = express()
-const port = 3306
+import express from 'express'
+import {getUsers} from './database.js'
 
-app.get('/', (req, res)=>{
-    res.send('hello world')
+const app = express()
+
+app.get('/test', (req, res) => {
+    res.send('test')
 })
 
-app.listen(port, ()=>{
-    console.log(`app is listening on port ${port}`);
+app.use((err, req, res, next)=>{
+    console.error(err.stack)
+    res.status(500).send('Something Broke!')
+})
+
+app.listen(3306, ()=>{
+    console.log('Server is running on port 3306');
 })
--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
 
+// assigns the pool variables to those in the .env file
 dotenv.config()
 
+// 'pool' variables for SQL database (uses 'promise' to prevent stalling)
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
@@ -10,10 +12,10 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
+// function to get users from database (uses 'await' to wait for 'pool' to load in)
 async function getUsers() {
     let [result] = await pool.query("SELECT * FROM users")
     return result;
 }
 
-let users = await getUsers()
-console.log(users);
+export {getUsers}
