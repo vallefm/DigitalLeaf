0bdaa28f2944b6ad9a21812f7ac3158cd447f527
syko99 <sykong.yang@hotmail.com>
2023-02-21 14:32:22 -0600

    basic front-end pages and components built for project/team pages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import bcrypt from "bcryptjs";
+import session from "express-session";
 
 //Router imports
 import { loginRouter } from "./routes/login.js";
@@ -9,13 +10,14 @@ import { logoutRouter } from "./routes/logout.js";
 import { signupRouter } from "./routes/signup.js";
 import { forgotPasswordRouter } from "./routes/forgot_Password.js";
 import { homeRouter } from "./routes/home.js";
-import session from "express-session";
+import { projectRouter } from "./routes/project.js"
+import { teamRouter } from "./routes/team.js";
 
 const app = express();
 const port = 8080;
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
-const secret = bcrypt.hashSync(bcrypt.genSaltSync());
+const secret = bcrypt.hashSync(bcrypt.genSaltSync())
 
 app.use(
   session({
@@ -24,27 +26,29 @@ app.use(
     resave: false,
     saveUninitialized: false,
   })
-);
+)
 
 // Load static files
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs")
 
 //convert requests to json, convert form requests objects to json
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Route handling
-app.use("/login", loginRouter);
-app.use("/signup", signupRouter);
-app.use("/forgotPassword", forgotPasswordRouter);
-app.use("/home", homeRouter);
+app.use("/login", loginRouter)
+app.use("/signup", signupRouter)
+app.use("/forgotPassword", forgotPasswordRouter)
+app.use("/home", homeRouter)
+app.use("/team", teamRouter)
+app.use("/project", projectRouter)
 app.use("/logout", logoutRouter)
 
 // Home route
 app.get("/", (req, res) => {
   if (!req.session.loggedIn) {
-    req.session.loggedIn = false;
+    req.session.loggedIn = false
   }
   res.render(path.join(__dirname + "/public/views/index"), {
     loggedIn: req.session.loggedIn,
@@ -54,10 +58,10 @@ app.get("/", (req, res) => {
 // Error page
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something Broke!");
+  res.status(500).send("Something Broke!")
 });
 
 // App listening
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`)
 });
---
+++ routes/team.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const router = express.Router()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+router.get('/', (req, res) => {
+    res.render(path.join(__dirname+'/../public/views/home_team'), {loggedIn: req.session.loggedIn, firstName: req.session.user.firstName})
+})
+
+export {router as teamRouter}
