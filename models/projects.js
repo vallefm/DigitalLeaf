2a3e5eeab7f0ab09c1608e7932d0a4b3d93e21bb
syko99 <sykong.yang@hotmail.com>
2023-02-23 21:23:08 -0600

    project update and task creation added.

--- models/projects.js
+++ models/projects.js
@@ -5,6 +5,10 @@
 
 import { conn } from "../connection.js";
 
+const projectPrefix = "prj";
+const updatePrefix = "upd";
+const taskPrefix = "tsk";
+
 async function getUserProjects(userId) {
   let [result] = await conn.query(
     "select projects_users.project_id as projectId, projects.name as projectName, projects.team_id as teamId, teams.name as teamName\
@@ -19,53 +23,145 @@ async function getUserProjects(userId) {
 }
 
 async function getProjectDetails(projectId) {
-  let [result] = await conn.query("select * from projects where id = ? limit 1", [projectId]);
+  let [result] = await conn.query(
+    "select * from projects where id = ? limit 1",
+    [projectId]
+  );
   return result;
 }
 
 async function createProject(projectName, description, dueDate, creatorId) {
-  let newProjectId = createProjectID()
-  description == '' ? description = null : description
-  dueDate == '' ? dueDate = null : dueDate 
-  let teamId = null
+  let newProjectId = createProjectID(projectPrefix);
+  description == "" ? (description = null) : description;
+  dueDate == "" ? (dueDate = null) : dueDate;
+  let teamId = null;
   try {
-    await conn.query("INSERT INTO projects VALUES (?, ?, ?, ?, ?, ?, ?, NOW())", [newProjectId, projectName, description, 0, teamId, dueDate, creatorId])
-    return true
+    await conn.query(
+      "INSERT INTO projects VALUES (?, ?, ?, ?, ?, ?, ?, NOW())",
+      [newProjectId, projectName, description, 0, teamId, dueDate, creatorId]
+    );
+    return true;
   } catch (error) {
     console.log(error);
-    return false
+    return false;
   }
 }
 
-function createProjectID() {
-  let prefix = 'prj'
-  let projectId = prefix+randomId()
-  while (!checkProjectExists(projectId)){
-    projectId = prefix+randomId()
+function createProjectID(prefix) {
+  let projectId = prefix + randomId();
+  while (!checkProjectExists(projectId)) {
+    projectId = prefix + randomId();
   }
-  return projectId
+  return projectId;
 }
 
 async function checkProjectExists(projectId) {
-  let [countData] = await conn.query("SELECT COUNT(*) as count FROM projects WHERE id = ?", [projectId])
-  let count = countData[0]["count"]
+  let [countData] = await conn.query(
+    "SELECT COUNT(*) as count FROM projects WHERE id = ?",
+    [projectId]
+  );
+  let count = countData[0]["count"];
   if (count != 0) {
-      return true;
+    return true;
   }
-  return false
+  return false;
 }
 
 async function deleteProject(projectId) {
-  if(checkProjectExists(projectId)){
-  try {
-    await conn.query("delete FROM projects WHERE id = ? limit 1", [projectId])
-    return true
-  } catch (error) {
-    return false
-  }
+  if (checkProjectExists(projectId)) {
+    try {
+      await conn.query("delete FROM projects WHERE id = ? limit 1", [
+        projectId,
+      ]);
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 }
 
+// Updates
+async function getProjectUpdates(projectId) {
+  let [result] = await conn.query(
+    "select * from projects_updates where project_id = ?",
+    [projectId]
+  );
+  return result;
+}
+
+async function createProjectUpdate(projectId, userId ,header, content) {
+  try {
+    await conn.query("INSERT INTO projects_updates VALUES (?, ?, ? , ?, NOW())", [
+      projectId,
+      userId,
+      header,
+      content,
+    ]);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+// Tasks
+async function getProjectTasks(projectId) {
+  let [result] = await conn.query("select * from tasks where project_id = ?", [
+    projectId,
+  ]);
+  return result;
+}
+
+async function createTask(title, dueDate, creatorId, projectId) {
+  let newTaskId = createTaskId(taskPrefix);
+  try {
+    await conn.query("INSERT INTO tasks VALUES (?, ?, ? , ?, ?, ?, NOW())", [
+      newTaskId,
+      title,
+      dueDate,
+      'new',
+      creatorId,
+      projectId
+    ]);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+function createTaskId(prefix) {
+  let taskId = prefix + randomId();
+  while (!checkTaskIdExists(taskId)) {
+    taskId = prefix + randomId();
+  }
+  return taskId;
+}
+
+async function checkTaskIdExists(taskId) {
+  let [countData] = await conn.query(
+    "SELECT COUNT(*) as count FROM tasks WHERE id = ?",
+    [taskId]
+  );
+  let count = countData[0]["count"];
+  if (count != 0) {
+    return true;
+  }
+  return false;
+}
+
+async function deleteTask(taskId) {
+  if (checkTaskIdExists(taskId)) {
+    try {
+      await conn.query("delete FROM tasks WHERE id = ? limit 1", [
+        taskId,
+      ]);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
 
 /**  generic random number generator up to 9-digits for id creation. Prepend relevant 3-letter tag to create complete id.
  * users: usr
@@ -73,13 +169,23 @@ async function deleteProject(projectId) {
  * projects: prj
  * tasks: tsk
  * subtask: stk
+ * update: upd
  * announcements: anc
  * message(for inbox): msg
  */
 
 function randomId() {
-  let id = Math.round(Math.random()*100000000)
+  let id = Math.round(Math.random() * 100000000);
   return id.toString();
 }
 
-export { createProject, getUserProjects, getProjectDetails, deleteProject};
+export {
+  createProject,
+  getUserProjects,
+  getProjectDetails,
+  deleteProject,
+  getProjectUpdates,
+  createProjectUpdate,
+  getProjectTasks,
+  createTask
+};
--- routes/project.js
+++ routes/project.js
@@ -1,49 +1,77 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { getProjectDetails, deleteProject } from "../models/projects.js";
+import {
+  getProjectDetails,
+  deleteProject,
+  getProjectUpdates,
+  createProjectUpdate,
+  getProjectTasks,
+  createTask
+} from "../models/projects.js";
 
 const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-
+// Project
 router.get("/:id", async (req, res) => {
   let [project] = await getProjectDetails(req.params.id);
-  let user = req.session.user
+  let updates = await getProjectUpdates(req.params.id);
+  let user = req.session.user;
   res.render(path.join(__dirname + "/../public/views/project"), {
     loggedIn: req.session.loggedIn,
     firstName: req.session.user.firstName,
+    user,
     project,
-    user
+    updates
   });
 });
 
+// Updates
+router.post('/:id/update', async (req, res) => {
+  let {header, content} = req.body
+  if(await createProjectUpdate(req.params.id, req.session.user.id, header, content)){
+      res.status(201).redirect(`/project/${req.params.id}`)
+  } 
+})
+
+// Tasks
 router.get("/:id/tasks", async (req, res) => {
   let [project] = await getProjectDetails(req.params.id);
-  let user = req.session.user
+  let tasks = await getProjectTasks(req.params.id);
+  let user = req.session.user;
   res.render(path.join(__dirname + "/../public/views/tasks"), {
     loggedIn: req.session.loggedIn,
     firstName: req.session.user.firstName,
     project,
-    user
+    user,
+    tasks
   });
 });
 
+router.post('/:id/tasks', async (req, res) => {
+  let {title, dueDate} = req.body
+  if(await createTask(title, dueDate, req.session.user.id, req.params.id)){
+      res.status(201).redirect(`/project/${req.params.id}/tasks`)
+  } 
+})
+
+// delete task
+
+// Delete project
 router.get("/:id/delete", async (req, res) => {
   let [project] = await getProjectDetails(req.params.id);
-  let user = req.session.user
+  let user = req.session.user;
   if (user.id == project.creator_id) {
     if (await deleteProject(project.id)) {
-      res.status(410).redirect(`/${user.firstName}/projects`)
+      res.status(410).redirect(`/${user.firstName}/projects`);
     } else {
-      res.status(404).redirect(`/${user.firstName}/projects`)
+      res.status(404).redirect(`/${user.firstName}/projects`);
     }
   } else {
-    res.status(401).redirect(`/${user.firstName}/projects`)
+    res.status(401).redirect(`/${user.firstName}/projects`);
   }
-
 });
 
 export { router as projectRouter };
