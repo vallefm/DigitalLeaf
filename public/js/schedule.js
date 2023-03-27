2d90bc9e006d4653348ed4e3fc4f92a5e628679d
beesal10-ce <65252604+beesal10-ce@users.noreply.github.com>
2023-03-27 14:10:34 -0500

    schedule

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,171 +1,481 @@
-function displaySchedule() {
-    var days = [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    var slots = [
-      "9:00 AM - 10:00 AM",
-      "10:00 AM - 11:00 AM",
-      "11:00 AM - 12:00 PM",
-      "12:00 PM - 1:00 PM",
-      "1:00 PM - 2:00 PM",
-      "2:00 PM - 3:00 PM",
-      "3:00 PM - 4:00 PM",
-      "4:00 PM - 5:00 PM",
-    ];
+// import express from "express";
+// import path from "path";
+// import { fileURLToPath } from "url";
+//import { createSchedule } from "../../models/schedule";
 
-    var table = document.createElement("table");
-    var thead = document.createElement("thead");
-    var tbody = document.createElement("tbody");
+// import {createSchedule} from "../../models/schedule"
 
-    // Create table header
-    var headerRow = document.createElement("tr");
-    var dayHeader = document.createElement("th");
-    dayHeader.appendChild(document.createTextNode("Day"));
-    headerRow.appendChild(dayHeader);
+// const router = express.Router();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-    for (var i = 0; i < slots.length; i++) {
-      var slotHeader = document.createElement("th");
-      slotHeader.appendChild(document.createTextNode(slots[i]));
-      headerRow.appendChild(slotHeader);
-    }
 
-    thead.appendChild(headerRow);
-    table.appendChild(thead);
+// router.post('/', async (req, res) => {
+//   let { eventTitle, timeFrom, timeTo, activeDay, month, year} = req.body
+//   let user = req.session.user;
+//   if(await createSchedule(user, eventTitle, timeFrom, timeTo, activeDay, month+1, year)){
+     
+//    console.log(req.body) 
+//   }
+// })
 
-    // Create table body
-    for (var i = 0; i < days.length; i++) {
-      var row = document.createElement("tr");
-      var dayCell = document.createElement("td");
-      dayCell.appendChild(document.createTextNode(days[i]));
-      row.appendChild(dayCell);
+// const modal = require('../../models/schedule.js');
 
-      for (var j = 0; j < slots.length; j++) {
-        var cell = document.createElement("td");
-        cell.setAttribute("id", days[i] + slots[j]);
-        var button = document.createElement("button");
-        button.setAttribute("type", "button");
-        button.setAttribute("class", "btn btn-sm btn-outline-primary");
-        button.setAttribute("data-toggle", "modal");
-        button.setAttribute(
-          "data-target",
-          "#" + days[i] + slots[j] + "Modal"
-        );
-        button.innerHTML = '<i class="fa fa-plus"></i>';
-        cell.appendChild(button);
 
-        var modal = document.createElement("div");
-        modal.setAttribute("class", "modal fade");
-        modal.setAttribute("id", days[i] + slots[j] + "Modal");
-        modal.setAttribute("tabindex", "-1");
-        modal.setAttribute("role", "dialog");
-        modal.setAttribute(
-          "aria-labelledby",
-          days[i] + slots[j] + "ModalLabel"
-        );
-        modal.setAttribute("aria-hidden", "true");
+const calendar = document.querySelector(".calendar"),
+  date = document.querySelector(".date"),
+  daysContainer = document.querySelector(".days"),
+  prev = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  todayBtn = document.querySelector(".today-btn"),
+  gotoBtn = document.querySelector(".goto-btn"),
+  dateInput = document.querySelector(".date-input"),
+  eventDay = document.querySelector(".event-day"),
+  eventDate = document.querySelector(".event-date"),
+  eventsContainer = document.querySelector(".events"),
+  addEventBtn = document.querySelector(".add-event"),
+  addEventWrapper = document.querySelector(".add-event-wrapper "),
+  addEventCloseBtn = document.querySelector(".close "),
+  addEventTitle = document.querySelector(".event-name "),
+  addEventFrom = document.querySelector(".event-time-from "),
+  addEventTo = document.querySelector(".event-time-to "),
+  addEventSubmit = document.querySelector(".add-event-btn ");
 
-        var modalDialog = document.createElement("div");
-        modalDialog.setAttribute("class", "modal-dialog");
-        modalDialog.setAttribute("role", "document");
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
 
-        var modalContent = document.createElement("div");
-        modalContent.setAttribute("class", "modal-content");
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-        var modalHeader = document.createElement("div");
-        modalHeader.setAttribute("class", "modal-header");
+// const eventsArr = [
+//   {
+//     day: 13,
+//     month: 11,
+//     year: 2022,
+//     events: [
+//       {
+//         title: "Event 1 lorem ipsun dolar sit genfa tersd dsad ",
+//         time: "10:00 AM",
+//       },
+//       {
+//         title: "Event 2",
+//         time: "11:00 AM",
+//       },
+//     ],
+//   },
+// ];
 
-        var modalTitle = document.createElement("h5");
-        modalTitle.setAttribute("class", "modal-title");
-        modalTitle.setAttribute("id", days[i] + slots[j] + "ModalLabel");
-        modalTitle.appendChild(document.createTextNode("Add Task"));
-        var closeButton = document.createElement("button");
-        closeButton.setAttribute("type", "button");
-        closeButton.setAttribute("class", "close");
-        closeButton.setAttribute("data-dismiss", "modal");
-        closeButton.setAttribute("aria-label", "Close");
+const eventsArr = [];
+getEvents();
+console.log(eventsArr);
 
-        var closeIcon = document.createElement("span");
-        closeIcon.setAttribute("aria-hidden", "true");
-        closeIcon.innerHTML = "&times;";
+//function to add days in days with class day and prev-date next-date on previous month and next month days and active on today
+function initCalendar() {
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const prevLastDay = new Date(year, month, 0);
+  const prevDays = prevLastDay.getDate();
+  const lastDate = lastDay.getDate();
+  const day = firstDay.getDay();
+  const nextDays = 7 - lastDay.getDay() - 1;
 
-        closeButton.appendChild(closeIcon);
-        modalHeader.appendChild(modalTitle);
-        modalHeader.appendChild(closeButton);
-        modalContent.appendChild(modalHeader);
+  date.innerHTML = months[month] + " " + year;
 
-        var modalBody = document.createElement("div");
-        modalBody.setAttribute("class", "modal-body");
+  let days = "";
 
-        var form = document.createElement("form");
-        form.setAttribute("id", days[i] + slots[j] + "Form");
+  for (let x = day; x > 0; x--) {
+    days += `<div class="day prev-date">${prevDays - x + 1}</div>`;
+  }
 
-        var formGroup = document.createElement("div");
-        formGroup.setAttribute("class", "form-group");
-
-        var label = document.createElement("label");
-        label.setAttribute("for", days[i] + slots[j] + "Input");
-        label.appendChild(document.createTextNode("Task"));
-
-        var input = document.createElement("input");
-        input.setAttribute("type", "text");
-        input.setAttribute("class", "form-control");
-        input.setAttribute("id", days[i] + slots[j] + "Input");
-
-        formGroup.appendChild(label);
-        formGroup.appendChild(input);
-        form.appendChild(formGroup);
-        modalBody.appendChild(form);
-        modalContent.appendChild(modalBody);
-
-        var modalFooter = document.createElement("div");
-        modalFooter.setAttribute("class", "modal-footer");
-
-        var saveButton = document.createElement("button");
-        saveButton.setAttribute("type", "button");
-        saveButton.setAttribute("class", "btn btn-primary");
-        saveButton.setAttribute(
-          "onclick",
-          'addTask("' + days[i] + slots[j] + '")'
-        );
-        saveButton.appendChild(document.createTextNode("Save"));
-
-        modalFooter.appendChild(saveButton);
-        modalContent.appendChild(modalFooter);
-        modalDialog.appendChild(modalContent);
-        modal.appendChild(modalDialog);
-        cell.appendChild(modal);
-        row.appendChild(cell);
+  for (let i = 1; i <= lastDate; i++) {
+    //check if event is present on that day
+    let event = false;
+    eventsArr.forEach((eventObj) => {
+      if (
+        eventObj.day === i &&
+        eventObj.month === month + 1 &&
+        eventObj.year === year
+      ) {
+        event = true;
       }
-      tbody.appendChild(row);
+    });
+    if (
+      i === new Date().getDate() &&
+      year === new Date().getFullYear() &&
+      month === new Date().getMonth()
+    ) {
+      activeDay = i;
+      getActiveDay(i);
+      updateEvents(i);
+      if (event) {
+        days += `<div class="day today active event">${i}</div>`;
+      } else {
+        days += `<div class="day today active">${i}</div>`;
+      }
+    } else {
+      if (event) {
+        days += `<div class="day event">${i}</div>`;
+      } else {
+        days += `<div class="day ">${i}</div>`;
+      }
     }
-
-    table.appendChild(tbody);
-
-    document.getElementById("schedule").appendChild(table);
   }
 
-  function addTask(id) {
-    var input = document.getElementById(id + "Input").value;
-    var cell = document.getElementById(id);
-    var task = document.createElement("div");
-    task.setAttribute("class", "rounded p-2 my-2");
-    task.style.backgroundColor = "#ffc107";
-    task.style.borderRadius = "30px";
-    task.appendChild(document.createTextNode(input));
-    task.style.transition = "transform 0.2s ease-in-out";
-
-    task.addEventListener("mouseenter", function () {
-      task.style.transform = "scale(1.1)";
-    });
-
-    task.addEventListener("mouseleave", function () {
-      task.style.transform = "scale(1)";
-    });
-    cell.appendChild(task);
-    $("#" + id + "Modal").modal("hide");
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="day next-date">${j}</div>`;
   }
+  daysContainer.innerHTML = days;
+  addListner();
+}
+
+//function to add month and year on prev and next button
+function prevMonth() {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  initCalendar();
+}
+
+function nextMonth() {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  initCalendar();
+}
+
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
+initCalendar();
+
+//function to add active on day
+function addListner() {
+  const days = document.querySelectorAll(".day");
+  days.forEach((day) => {
+    day.addEventListener("click", (e) => {
+      getActiveDay(e.target.innerHTML);
+      updateEvents(Number(e.target.innerHTML));
+      activeDay = Number(e.target.innerHTML);
+      //remove active
+      days.forEach((day) => {
+        day.classList.remove("active");
+      });
+      //if clicked prev-date or next-date switch to that month
+      if (e.target.classList.contains("prev-date")) {
+        prevMonth();
+        //add active to clicked day afte month is change
+        setTimeout(() => {
+          //add active where no prev-date or next-date
+          const days = document.querySelectorAll(".day");
+          days.forEach((day) => {
+            if (
+              !day.classList.contains("prev-date") &&
+              day.innerHTML === e.target.innerHTML
+            ) {
+              day.classList.add("active");
+            }
+          });
+        }, 100);
+      } else if (e.target.classList.contains("next-date")) {
+        nextMonth();
+        //add active to clicked day afte month is changed
+        setTimeout(() => {
+          const days = document.querySelectorAll(".day");
+          days.forEach((day) => {
+            if (
+              !day.classList.contains("next-date") &&
+              day.innerHTML === e.target.innerHTML
+            ) {
+              day.classList.add("active");
+            }
+          });
+        }, 100);
+      } else {
+        e.target.classList.add("active");
+      }
+    });
+  });
+}
+
+todayBtn.addEventListener("click", () => {
+  today = new Date();
+  month = today.getMonth();
+  year = today.getFullYear();
+  initCalendar();
+});
+
+dateInput.addEventListener("input", (e) => {
+  dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+  if (dateInput.value.length === 2) {
+    dateInput.value += "/";
+  }
+  if (dateInput.value.length > 7) {
+    dateInput.value = dateInput.value.slice(0, 7);
+  }
+  if (e.inputType === "deleteContentBackward") {
+    if (dateInput.value.length === 3) {
+      dateInput.value = dateInput.value.slice(0, 2);
+    }
+  }
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+  console.log("here");
+  const dateArr = dateInput.value.split("/");
+  if (dateArr.length === 2) {
+    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+      month = dateArr[0] - 1;
+      year = dateArr[1];
+      initCalendar();
+      return;
+    }
+  }
+  alert("Invalid Date");
+}
+
+//function get active day day name and date and update eventday eventdate
+function getActiveDay(date) {
+  const day = new Date(year, month, date);
+  const dayName = day.toString().split(" ")[0];
+  eventDay.innerHTML = dayName;
+  eventDate.innerHTML = date + " " + months[month] + " " + year;
+}
+
+//function update events when a day is active
+function updateEvents(date) {
+  let events = "";
+  eventsArr.forEach((event) => {
+    if (
+      date === event.day &&
+      month + 1 === event.month &&
+      year === event.year
+    ) {
+      event.events.forEach((event) => {
+        events += `<div class="event">
+        <div class="title">
+          <i class="fas fa-circle"></i>
+          <h3 class="event-title">${event.title}</h3>
+        </div>
+        <div class="event-time">
+          <span class="event-time">${event.time}</span>
+        </div>
+    </div>`;
+      });
+    }
+  });
+  if (events === "") {
+    events = `<div class="no-event">
+        <h3>No Events</h3>
+    </div>`;
+  }
+  eventsContainer.innerHTML = events;
+  saveEvents();
+}
+
+//function to add event
+addEventBtn.addEventListener("click", () => {
+  addEventWrapper.classList.toggle("active");
+});
+
+addEventCloseBtn.addEventListener("click", () => {
+  addEventWrapper.classList.remove("active");
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target !== addEventBtn && !addEventWrapper.contains(e.target)) {
+    addEventWrapper.classList.remove("active");
+  }
+});
+
+//allow 50 chars in eventtitle
+addEventTitle.addEventListener("input", (e) => {
+  addEventTitle.value = addEventTitle.value.slice(0, 60);
+});
+
+//allow only time in eventtime from and to
+addEventFrom.addEventListener("input", (e) => {
+  addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
+  if (addEventFrom.value.length === 2) {
+    addEventFrom.value += ":";
+  }
+  if (addEventFrom.value.length > 5) {
+    addEventFrom.value = addEventFrom.value.slice(0, 5);
+  }
+});
+
+addEventTo.addEventListener("input", (e) => {
+  addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, "");
+  if (addEventTo.value.length === 2) {
+    addEventTo.value += ":";
+  }
+  if (addEventTo.value.length > 5) {
+    addEventTo.value = addEventTo.value.slice(0, 5);
+  }
+});
+
+//function to add event to eventsArr
+addEventSubmit.addEventListener("click", () => {
+  const eventTitle = addEventTitle.value;
+  const eventTimeFrom = addEventFrom.value;
+  const eventTimeTo = addEventTo.value;
+  if (eventTitle === "" || eventTimeFrom === "" || eventTimeTo === "") {
+    alert("Please fill all the fields");
+    return;
+  }
+
+  //check correct time format 24 hour
+  const timeFromArr = eventTimeFrom.split(":");
+  const timeToArr = eventTimeTo.split(":");
+  if (
+    timeFromArr.length !== 2 ||
+    timeToArr.length !== 2 ||
+    timeFromArr[0] > 23 ||
+    timeFromArr[1] > 59 ||
+    timeToArr[0] > 23 ||
+    timeToArr[1] > 59
+  ) {
+    alert("Invalid Time Format");
+    return;
+  }
+
+  const timeFrom = convertTime(eventTimeFrom);
+  const timeTo = convertTime(eventTimeTo);
+
+  //check if event is already added
+  let eventExist = false;
+  eventsArr.forEach((event) => {
+    if (
+      event.day === activeDay &&
+      event.month === month + 1 &&
+      event.year === year
+    ) {
+      event.events.forEach((event) => {
+        if (event.title === eventTitle) {
+          eventExist = true;
+        }
+      });
+    }
+  });
+  if (eventExist) {
+    alert("Event already added");
+    return;
+  }
+  const newEvent = {
+    title: eventTitle,
+    time: timeFrom + " - " + timeTo,
+  };
+  console.log(newEvent);
+  console.log(activeDay);
+  let eventAdded = false;
+  if (eventsArr.length > 0) {
+    eventsArr.forEach((item) => {
+      if (
+        item.day === activeDay &&
+        item.month === month + 1 &&
+        item.year === year
+      ) {
+        item.events.push(newEvent);
+        eventAdded = true;
+      }
+    });
+  }
+
+  if (!eventAdded) {
+ modal.createSchedule("usr4337041", eventTitle, timeFrom, timeTo, activeDay, month+1, year);
+    eventsArr.push({
+      day: activeDay,
+      month: month + 1,
+      year: year,
+      events: [newEvent],
+    });
+  }
+
+  console.log(eventsArr);
+  addEventWrapper.classList.remove("active");
+  addEventTitle.value = "";
+  addEventFrom.value = "";
+  addEventTo.value = "";
+  updateEvents(activeDay);
+  //select active day and add event class if not added
+  const activeDayEl = document.querySelector(".day.active");
+  if (!activeDayEl.classList.contains("event")) {
+    activeDayEl.classList.add("event");
+  }
+});
+
+//function to delete event when clicked on event
+eventsContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("event")) {
+    if (confirm("Are you sure you want to delete this event?")) {
+      const eventTitle = e.target.children[0].children[1].innerHTML;
+      eventsArr.forEach((event) => {
+        if (
+          event.day === activeDay &&
+          event.month === month + 1 &&
+          event.year === year
+        ) {
+          event.events.forEach((item, index) => {
+            if (item.title === eventTitle) {
+              event.events.splice(index, 1);
+            }
+          });
+          //if no events left in a day then remove that day from eventsArr
+          if (event.events.length === 0) {
+            eventsArr.splice(eventsArr.indexOf(event), 1);
+            //remove event class from day
+            const activeDayEl = document.querySelector(".day.active");
+            if (activeDayEl.classList.contains("event")) {
+              activeDayEl.classList.remove("event");
+            }
+          }
+        }
+      });
+      updateEvents(activeDay);
+    }
+  }
+});
+
+//function to save events in local storage
+function saveEvents() {
+  localStorage.setItem("events", JSON.stringify(eventsArr));
+}
+
+//function to get events from local storage
+function getEvents() {
+  //check if events are already saved in local storage then return event else nothing
+  if (localStorage.getItem("events") === null) {
+    return;
+  }
+  eventsArr.push(...JSON.parse(localStorage.getItem("events")));
+}
+
+function convertTime(time) {
+  //convert time to 24 hour format
+  let timeArr = time.split(":");
+  let timeHour = timeArr[0];
+  let timeMin = timeArr[1];
+  let timeFormat = timeHour >= 12 ? "PM" : "AM";
+  timeHour = timeHour % 12 || 12;
+  time = timeHour + ":" + timeMin + " " + timeFormat;
+  return time;
+}
--- routes/schedule.js
+++ routes/schedule.js
@@ -2,6 +2,9 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+import { createSchedule } from "../../models/schedule";
+
+
 const router = express.Router()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
