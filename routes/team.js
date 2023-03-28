819c2ec5bc3644c0566ebbd16fd0cc73d933aedf
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2023-03-28 18:37:39 -0500

    more testing of teams

--- routes/team.js
+++ routes/team.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 import { getUserInfo } from "../models/teams.js"
 
 /*
-router.get('/team', async(req, res) => {
+router.get('/:id/teams', async(req, res) => {
     res.render(path.join(__dirname+'/../public/views/team'), {
         loggedIn: req.session.loggedIn,
         firstName: req.session.user.firstName,
@@ -21,10 +21,26 @@ router.get('/team', async(req, res) => {
 // Router for 'team tab' within a project card
 router.get('/teams', async (req, res) => {
     let teams = await getUserInfo(req.session.user.id)
+    let searchUser = await getUserInfo(req.session.user.id)
     res.render(path.join(__dirname+'/../public/views/teams'), {
     loggedIn: req.session.loggedIn,
     firstName: req.session.user.firstName,
-    teams});
+    teams,
+    searchUser
+    });
 });
+
+
+router.get('/search/:searchUser', async (req, res) => {
+    let searchUser = await getUserInfo(req.session.user.id)
+    res.render(path.join(__dirname+'/../public/views/teams'), {
+        loggedIn: req.session.loggedIn,
+        firstName: req.session.user.firstName,
+        searchUser
+    });
+});
+
+
+
 
 export {router as teamRouter}
