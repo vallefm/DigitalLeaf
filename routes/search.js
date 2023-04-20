277fc82b0a9240090d1817cd28d4d87fc71d0f3e
syko99 <sykong.yang@hotmail.com>
2023-04-20 14:29:25 -0500

    user can now add subtasks. updated user search routing to fix action bar context

--- routes/search.js
+++ routes/search.js
@@ -6,38 +6,37 @@ const router = express.Router()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-import { getUserInfo } from "../models/search.js"
-import { addProjectMember } from '../models/projects.js'
+import { getUserInfo } from '../models/search.js'
+import { addProjectMember, getProjectDetails } from '../models/projects.js'
 
 // Router for 'team tab' within a project card
 router.get('/:projectId', async (req, res) => {
-    let projectId = req.params.projectId
-    res.render(path.join(__dirname+'/../public/views/search'), {
-    loggedIn: req.session.loggedIn,
-    firstName: req.session.user.firstName,
-    projectId
-    });
-});
-
+    let [project] = await getProjectDetails(req.params.projectId)
+    res.render(path.join(__dirname + '/../public/views/search'), {
+        loggedIn: req.session.loggedIn,
+        firstName: req.session.user.firstName,
+        project,
+    })
+})
 
 router.post('/:projectId/searchUsers', async (req, res) => {
-    let projectId = req.params.projectId
+    let [project] = await getProjectDetails(req.params.projectId)
     let searchUsers = await getUserInfo(req.body.searchUser)
-    res.render(path.join(__dirname+'/../public/views/search'), {
+    res.render(path.join(__dirname + '/../public/views/search'), {
         loggedIn: req.session.loggedIn,
         firstName: req.session.user.firstName,
         searchUsers,
-        projectId
-    });
-    
-});
+        project,
+    })
+})
 
 router.get('/:projectId/searchUsers/add/:userId', async (req, res) => {
     let projectId = req.params.projectId
     let userId = req.params.userId
     let result = await addProjectMember(projectId, userId)
     if (result == true) {
-    res.redirect(`/project/${projectId}`)}});
+        res.redirect(`/project/${projectId}`)
+    }
+})
 
-
-export {router as searchRouter}
+export { router as searchRouter }
